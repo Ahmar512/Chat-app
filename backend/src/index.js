0a9ceef532bc77073bf1af714c6b703f98dc96c4
Ahmar Ansari <ahmaransari29@gmail.com>
2026-07-18 14:44:27 +0530

    feat: initialize backend server with socket.io support and frontend project structure

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,8 +14,8 @@ const PORT = process.env.PORT;
 app.use(express.json({limit: '50mb'}));
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials:true,
+    origin: true,
+    credentials: true,
 }))
 
 app.use('/api/auth', authRouter);
