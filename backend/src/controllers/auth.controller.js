29918806cf5878e0ad398d9b958206ae4f3f55e8
Ahmar Ansari <ahmaransari29@gmail.com>
2026-09-06 07:16:13 +0530

    seperate backend and frontend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -83,8 +83,13 @@ export const login = async (req, res) =>{
 }
 export const logout = (req, res) =>{
     try {
-        res.cookie("jwt", "", {maxAge:0});
-        res.status(201).json({message:"Log out Successfully"});
+        res.cookie("jwt", "", {
+            maxAge: 0,
+            httpOnly: true,
+            sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+            secure: process.env.NODE_ENV !== "development"
+        });
+        res.status(200).json({message:"Logged out successfully"});
     } catch (error) {
         console.log("error is logout controller:", error);
         res.status(500).json({message:"Internal Server Error"});
