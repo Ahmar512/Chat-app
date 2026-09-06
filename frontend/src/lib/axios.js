a05cc9d359ce3c01476cccea2f521a45de28f8a3
Ahmar Ansari <ahmaransari29@gmail.com>
2026-09-06 07:29:24 +0530

    changes env variable name in frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL || (import.meta.env.MODE === "development" ? "http://localhost:5001" : "");
+const backendUrl = 
+    import.meta.env.VITE_BACKEND_URL || 
+    import.meta.env.BACKEND_URL || 
+    (import.meta.env.MODE === "development" ? "http://localhost:5009" : "");
 
 export const axiosInstance = axios.create({
     baseURL: `${backendUrl}/api`,
